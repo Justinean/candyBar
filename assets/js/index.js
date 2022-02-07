0bd1bc196ba311fe7e49413d1db91afa3b64d678
Justin Hoch <justinhoch22@gmail.com>
2022-02-07 03:37:20 -0600

    fixed styling and added a reset button

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,9 @@ var wrapper = document.querySelector(".wrapper");
 var wrapperText = document.querySelector(".wrapperText");
 var chocolate = document.querySelector(".chocolate");
 var biteContainer = document.querySelector(".biteContainer");
+var chocolateText = document.querySelector(".chocolateText");
+var openText = document.querySelector(".openText");
+var resetButton = document.querySelector(".resetButton");
 var chompCount = 0;
 var displayWrapper = true;
 var jsMediaQuery = function () {
@@ -9,15 +12,16 @@ var jsMediaQuery = function () {
         wrapper.style.width = "".concat(window.innerWidth * .9, "px");
         wrapper.style.height = "".concat(parseInt(wrapper.style.width) / 3, "px");
         wrapperText.style.fontSize = "50px";
+        chocolateText.style.fontSize = "50px";
         chocolate.style.width = "".concat(window.innerWidth * .9, "px");
         chocolate.style.height = "".concat(parseInt(chocolate.style.width) / 3 + 25, "px");
         biteContainer.style.width = "".concat(parseInt(chocolate.style.width) + (parseInt(chocolate.style.width) / 12), "px");
-        console.log(parseInt(chocolate.style.width) + (parseInt(chocolate.style.width) / 24));
     }
     else {
         wrapper.style.width = "600px";
         wrapper.style.height = "".concat(parseInt(wrapper.style.width) / 3, "px");
-        wrapperText.style.fontSize = "70px";
+        wrapperText.style.fontSize = "80px";
+        chocolateText.style.fontSize = "80px";
         chocolate.style.width = "600px";
         chocolate.style.height = "".concat(parseInt(chocolate.style.width) / 3, "px");
         biteContainer.style.width = "650px";
@@ -32,11 +36,12 @@ var createBite = function () {
 };
 var clickHandler = function (e) {
     if (e.target === wrapper || e.target === wrapperText || e.target === biteContainer) {
+        openText.textContent = "Click to eat";
         wrapper.style.backgroundColor = "transparent";
         wrapperText.style.display = "none";
-        chocolate.style.display = "block";
+        chocolate.style.display = "flex";
     }
-    else if (e.target === chocolate || e.target === biteContainer) {
+    else if (e.target === chocolate || e.target === biteContainer || e.target === chocolate.children[0]) {
         var chomp = createBite();
         biteContainer.appendChild(chomp);
         var row = 0;
@@ -56,6 +61,22 @@ var clickHandler = function (e) {
             }
         }
         chompCount++;
+        if (chompCount >= 40) {
+            openText.textContent = "Click button to reset chocolate";
+            resetButton.style.display = "block";
+        }
+    }
+    else if (e.target === resetButton && chompCount >= 40) {
+        openText.textContent = "Click to open";
+        wrapper.style.backgroundColor = "black";
+        wrapperText.style.display = "block";
+        chocolate.style.display = "none";
+        resetButton.style.display = "none";
+        chompCount = 0;
+        var childLength = biteContainer.children.length;
+        for (var i = 0; i < childLength; i++) {
+            biteContainer.removeChild(biteContainer.children[0]);
+        }
     }
 };
 document.addEventListener("click", clickHandler);
